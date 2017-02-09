0857dfea54f6de52592eb7700fc76036dc206309
FSEVERI\crivellaro3452 <FSEVERI\crivellaro3452@linf1-12.fseveri.it>
2017-02-09 13:16:04 +0100

    First Commit

---
+++ Rubrica/src/pacchettoRubrica/Voce.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacchettoRubrica;
+
+/**
+ *
+ * @author FSEVERI\crivellaro3452
+ */
+public class Voce {
+    private String nome;
+    private String nTel;
+    
+}
